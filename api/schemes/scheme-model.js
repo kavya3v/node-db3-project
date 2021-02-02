f041c02180d8bb677e8fdb093d825a639b82188b
KVlearn <kavya.mdu@gmail.com>
2021-02-01 21:51:13 -0800

    completed

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,1 +1,61 @@
 // scheme-model
+const db=require('../../data/db-config');
+
+module.exports={find,findById,findSteps,add,addStep,update,remove}
+
+async function find(){
+const schemes= await db('schemes')
+return schemes;
+}
+
+async function findById(id){
+    const scheme= await db('schemes').where({id}).first()
+    return scheme
+}
+
+//expects schema id
+async function findSteps(id){
+    console.log('id',id)
+//    const steps= await db('steps as st')
+//                 // .innerJoin('schemes as sc' , 'sc.id', 'st.scheme_id')
+//                 // .where('scheme_id',id)
+//                 // .select('sc.scheme_name','st.id','st.step_number', 'st.instructions')
+//                 // .orderBy('st.step_number');
+
+    const steps= await db('steps as st')
+                        .innerJoin('schemes as sc','sc.id','st.scheme_id')
+                        .where('st.scheme_id',id)
+                        .select('st.id','sc.scheme_name','st.step_number','st.instructions')
+                        .orderBy('st.step_number')
+                    
+    return steps;
+}
+
+async function add(scheme){
+ const schemeId=  await db('schemes').insert(scheme)
+ const newScheme= await findById(schemeId);
+ return newScheme;
+}
+
+async function addStep(stepData, scheme_id){
+  const newStep={...stepData,
+             scheme_id:scheme_id}
+  const stepId= await db('steps')
+                 .insert(newStep)
+  const SchemeSteps= await findSteps(scheme_id);
+ return SchemeSteps;
+}
+
+
+async function update(changes,id){
+    const updateCount= await db('schemes')
+                    .where({id})
+                    .update(changes)
+    const updates= await findById(id);
+    return updates;
+}
+
+async function remove(id){
+    const count= await db('schemes').where({id}).del()
+    return count;
+}
--- queries.sql
+++ queries.sql
@@ -1,9 +1,45 @@
 -- Multi-Table Query Practice
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+select c.CategoryName, p.ProductName
+from Category as c
+join Product as p
+on p.CategoryId= c.id;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+select o.Id as orderId , s.CompanyName
+from [Order] as o
+join Shipper as s
+on o.shipVia = s.id
+where o.OrderDate < '2012-08-09'
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
+select o.Quantity, p.ProductName from 
+OrderDetail as o
+join Product as p
+on p.id = o.productId
+where o.OrderId = 10251
+ORDER BY p.ProductName
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+ select o.Id as 'Order Id',
+ c.CompanyName 'Customer Company Name',
+ e.LastName as 'Employee LastName'
+ from [Order] as o
+ join Customer as c
+ on o.customerId = c.id
+ join Employee as e
+ on o.employeeId = e.id
+
+ --Displays CategoryName and a new column called Count that shows how many products are in each category. Shows 9 records.
+
+SELECT CategoryName , count() as count
+ from Categories as c
+ join Products as p
+ on c.CategoryID = p.CategoryID
+ Group By CategoryName
+
+--Display OrderID and a column called ItemCount that shows the total number of products placed on the order. Shows 196 records.
+SELECT OrderID, sum(Quantity) as ItemCount
+FROM [OrderDetails]
+Group by OrderID
